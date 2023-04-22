062a56d0218f80026a2d86b96c48ff1e9d979b04
Lisa Hang <lisahang13@gmail.com>
2023-04-22 12:22:47 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(record) {
+    let findWinner = record.find(({result}) => result === "W");
+        if (findWinner) {
+            return findWinner["year"];
+        }
+}
